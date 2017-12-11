ca45707f7cf950e35e442129852be114892aba1a
zylgg <move20150109@sina.cn>
2017-12-11 18:41:12 +0800

    继承BaseActivity

--- app/src/main/java/com/example/mr_zyl/project/BaseApplication.java
+++ app/src/main/java/com/example/mr_zyl/project/BaseApplication.java
@@ -72,7 +72,7 @@ public class BaseApplication extends Application {
     private void initImagePicker() {
         ImagePicker imagePicker = ImagePicker.getInstance();
         imagePicker.setImageLoader(new UILImageLoader());   //设置图片加载器
-        imagePicker.setShowCamera(false);  //显示拍照按钮
+        imagePicker.setShowCamera(true);  //显示拍照按钮
         imagePicker.setCrop(false);        //允许裁剪（单选才有效）
         imagePicker.setSaveRectangle(false); //是否按矩形区域保存
         imagePicker.setSelectLimit(9);    //选中数量限制
