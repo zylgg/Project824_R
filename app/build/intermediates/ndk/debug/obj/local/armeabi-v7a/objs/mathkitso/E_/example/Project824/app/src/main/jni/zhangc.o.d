9ce8cd20bde7bf3d815ef6e3b0b6d519f507bff4
zyl <move20150109@sina.cn>
2018-03-07 20:28:44 +0800

    sss

--- app/build/intermediates/ndk/debug/obj/local/armeabi-v7a/objs/mathkitso/E_/example/Project824/app/src/main/jni/zhangc.o.d
+++
@@ -1,2 +0,0 @@
-E:\example\Project824\app\build\intermediates\ndk\debug\obj/local/armeabi-v7a/objs/mathkitso/E_\example\Project824\app\src\main\jni\zhangc.o: \
-  E:\example\Project824\app\src\main\jni\zhangc.c
--- app/build/intermediates/ndk/debug/obj/local/x86/objs/mathkitso/E_/example/Project824/app/src/main/jni/zhangc.o.d
+++
@@ -1,2 +0,0 @@
-E:\example\Project824\app\build\intermediates\ndk\debug\obj/local/x86/objs/mathkitso/E_\example\Project824\app\src\main\jni\zhangc.o: \
-  E:\example\Project824\app\src\main\jni\zhangc.c
