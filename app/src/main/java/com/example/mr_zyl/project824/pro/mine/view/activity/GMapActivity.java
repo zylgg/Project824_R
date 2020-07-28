2129f77aa34649b1fbb00dc9aa9540f143e14890
zyl <move20150109@sina.cn>
2020-07-28 19:08:50 +0800

    修改搞得地图定位适当偏移

--- app/src/main/java/com/example/mr_zyl/project824/pro/mine/view/activity/GMapActivity.java
+++ app/src/main/java/com/example/mr_zyl/project824/pro/mine/view/activity/GMapActivity.java
@@ -3,17 +3,29 @@ package com.example.mr_zyl.project824.pro.mine.view.activity;
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-import android.graphics.Color;
+import android.graphics.Point;
 import android.location.Location;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.CoordinatorLayout;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.view.animation.LinearInterpolator;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+
 import com.amap.api.maps.AMap;
-import com.amap.api.maps.MapView;
+import com.amap.api.maps.CameraUpdateFactory;
+import com.amap.api.maps.TextureMapView;
+import com.amap.api.maps.UiSettings;
 import com.amap.api.maps.model.BitmapDescriptorFactory;
 import com.amap.api.maps.model.LatLng;
 import com.amap.api.maps.model.Marker;
@@ -23,6 +35,9 @@ import com.amap.api.maps.model.animation.Animation;
 import com.amap.api.maps.model.animation.RotateAnimation;
 import com.example.mr_zyl.project824.R;
 import com.example.mr_zyl.project824.pro.base.view.BaseActivity;
+import com.example.mr_zyl.project824.pro.mine.view.selfview.MapContainer;
+import com.example.mr_zyl.project824.utils.DensityUtil;
+import com.example.mr_zyl.project824.utils.ToastUtil;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -30,42 +45,86 @@ import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import cn.bingoogolapple.bgabanner.BGABanner;
 
 public class GMapActivity extends BaseActivity implements AMap.OnMyLocationChangeListener {
 
     private static final String TAG = "GMapActivity";
-    @BindView(R.id.mv_gmap)
-    MapView mv_gmap;
+    @BindView(R.id.tmv_current_oilStation)
+    TextureMapView tmv_current_oilStation;
     private AMap aMap;
+    /**
+     * 头部区域
+     */
+    @BindView(R.id.ll_homeTopLayout)
+    LinearLayout ll_homeTopLayout;
+    /**
+     * 顶部轮播图
+     */
+    @BindView(R.id.banner_home_top)
+    BGABanner bannerHomeTop;
+    /**
+     * 加油地图
+     */
+    @BindView(R.id.rl_oilstation_mapContent)
+    RelativeLayout rl_oilstation_mapContent;
+    @BindView(R.id.mc_gmap_container)
+    MapContainer mc_gmap_container;
+    /**
+     * 一键加油卡片区域
+     */
+    @BindView(R.id.ll_oilstatiion_contentLayout)
+    LinearLayout ll_oilstatiion_contentLayout;
+    /**
+     * 更多网点
+     */
+    @BindView(R.id.cv_oilstation_topdescMore)
+    CardView cv_oilstation_topdescMore;
+    @BindView(R.id.appbar_home)
+    AppBarLayout appbarHome;
+
+
+    // 下拉刷新
+
     private Marker marker;
 
+    @Override
+    protected int initLayoutId() {
+        return R.layout.activity_gmap;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mv_gmap.onCreate(savedInstanceState);
+        tmv_current_oilStation.onCreate(savedInstanceState);
         Log.i(TAG, "当前编译的APK的SHA1: " + sHA1(this));
+
         //获取地图对象
-        aMap = mv_gmap.getMap();
+        aMap = tmv_current_oilStation.getMap();
+        //处理滑动冲突
+        mc_gmap_container.setScrollView(appbarHome);
         //设置定位属性
         MyLocationStyle style = new MyLocationStyle();
         style.showMyLocation(true);
-//        style.strokeColor(Color.TRANSPARENT);
-        style.radiusFillColor(Color.TRANSPARENT);
-        style.interval(2500);
-        style.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE_NO_CENTER);
+        style.myLocationType(MyLocationStyle.LOCATION_TYPE_SHOW);
 
          //设置定位样式并开始定位
         aMap.setMyLocationStyle(style);
-        aMap.getUiSettings().setMyLocationButtonEnabled(true);// 设置默认定位按钮是否显示
+        UiSettings uiSettings = aMap.getUiSettings();
+        uiSettings.setZoomControlsEnabled(false);//关闭放大 缩小ui
+        uiSettings.setMyLocationButtonEnabled(false);// 关闭默认定位按钮ui
+        uiSettings.setCompassEnabled(false);//关闭指南针
+        uiSettings.setScaleControlsEnabled(false);//控制比例尺控件是否显示
+
         aMap.setMyLocationEnabled(true);
         aMap.setOnMyLocationChangeListener(this);
+
+
         //创建覆盖物
         MarkerOptions markerOptions = new MarkerOptions();
         markerOptions.position(new LatLng(39.906901, 116.397972))
                 .title("北京")
                 .draggable(true)
-//                .infoWindowEnable(false)
                 .icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_marka))
                 .snippet("默认的marker");
         marker = aMap.addMarker(markerOptions);
@@ -78,10 +137,16 @@ public class GMapActivity extends BaseActivity implements AMap.OnMyLocationChang
         //自定义infowindow
         aMap.setInfoWindowAdapter(new AMap.InfoWindowAdapter() {
             @Override
-            public View getInfoWindow(Marker marker) {
+            public View getInfoWindow(final Marker marker) {
                 View  view= LayoutInflater.from(GMapActivity.this).inflate(R.layout.activity_amap_infowindow,null);
-                TextView Title = ButterKnife.findById(view, R.id.Title);
-                TextView Content=ButterKnife.findById(view,R.id.Content);
+                TextView Title =view.findViewById(R.id.Title);
+                Title.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        ToastUtil.showToast(GMapActivity.this,marker.getTitle());
+                    }
+                });
+                TextView Content=view.findViewById(R.id.Content);
                 Title.setText(marker.getTitle());
                 Content.setText(marker.getSnippet());
                 return view;
@@ -92,16 +157,66 @@ public class GMapActivity extends BaseActivity implements AMap.OnMyLocationChang
                 return null;
             }
         });
+        initListener();
+    }
+
+
+    /**
+     * 中间空出的地图高度
+     */
+    private int spaceMapH = 0;
+    /**
+     * 初始化监听
+     */
+    private void initListener() {
+//        initAppbar();
+        appbarHome.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int i) {
+                float offsetY = Math.abs(i * 1.0f);
+                int TopStartScrollY = spaceMapH;//空出的地图高度
+                if (i < -TopStartScrollY) {//例如：-151<-150
+                    ll_homeTopLayout.setTranslationY(i + TopStartScrollY);
+                    rl_oilstation_mapContent.setTranslationY(i + TopStartScrollY);
+                } else {
+                    float rate = offsetY / TopStartScrollY;//设置提示语布局渐变openGaoDeMap
+                    cv_oilstation_topdescMore.setAlpha(1 - rate);
+
+                    ll_homeTopLayout.setTranslationY(0);
+                    rl_oilstation_mapContent.setTranslationY(0);
+                }
+
+            }
+        });
+        //计算轮播图和金刚区的总高度
+        bannerHomeTop.getViewTreeObserver().addOnGlobalLayoutListener(
+                new ViewTreeObserver.OnGlobalLayoutListener() {
+                    @Override
+                    public void onGlobalLayout() {
+                        //只需要获取一次高度，获取后移除监听器
+                        bannerHomeTop.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                        int height = bannerHomeTop.getHeight();//轮播图高度
+
+                        //设置一键加油地图 上边距
+                        CollapsingToolbarLayout.LayoutParams layoutParams = (CollapsingToolbarLayout.LayoutParams) rl_oilstation_mapContent.getLayoutParams();
+                        int marginTop1 = height + DensityUtil.dp2px(68);//轮播图➕金刚区高度
+                        layoutParams.topMargin = marginTop1;
+                        rl_oilstation_mapContent.setLayoutParams(layoutParams);
+
+                        //设置一键加油卡片 上边距
+                        CollapsingToolbarLayout.LayoutParams layoutParams2 = (CollapsingToolbarLayout.LayoutParams) ll_oilstatiion_contentLayout.getLayoutParams();
+                        spaceMapH = rl_oilstation_mapContent.getHeight() - ll_oilstatiion_contentLayout.getChildAt(0).getHeight(); //空出的地图区域(地图高-地图上卡片高)
+                        layoutParams2.topMargin = marginTop1 + spaceMapH;//轮播图➕金刚区高度➕空出的地图区域(地图高-地图上卡片高)
+                        ll_oilstatiion_contentLayout.setLayoutParams(layoutParams2);
+                    }
+
+                });
+
+
         //设置覆盖物监听
         aMap.setOnMarkerClickListener(new AMap.OnMarkerClickListener() {
             @Override
             public boolean onMarkerClick(Marker marker) {
-                if (marker.isInfoWindowShown()){
-                    marker.hideInfoWindow();
-                }else{
-                    marker.showInfoWindow();
-                }
-                Log.i(TAG, "onMarkerClick: " + marker.getPosition().latitude+"——"+ marker.getPosition().longitude);
                 return false;
             }
         });
@@ -109,11 +224,11 @@ public class GMapActivity extends BaseActivity implements AMap.OnMyLocationChang
         aMap.setOnMapClickListener(new AMap.OnMapClickListener() {
             @Override
             public void onMapClick(LatLng latLng) {
-                if (marker.isInfoWindowShown()){
-                    marker.hideInfoWindow();
-                }else{
-                    marker.showInfoWindow();
-                }
+//                if (marker.isInfoWindowShown()){
+//                    marker.hideInfoWindow();
+//                }else{
+//                    marker.showInfoWindow();
+//                }
             }
         });
         //设置地图的点击事件
@@ -129,33 +244,95 @@ public class GMapActivity extends BaseActivity implements AMap.OnMyLocationChang
         });
     }
 
+
     @Override
-    protected int initLayoutId() {
-        return R.layout.activity_gmap;
+    public void onMyLocationChange(Location location) {
+        // 定位回调监听
+        if (location != null) {
+            Log.e("onMyLocationChange", "onMyLocationChange 定位成功， lat: " + location.getLatitude() + " lon: " + location.getLongitude());
+            Bundle bundle = location.getExtras();
+            if (bundle != null) {
+                int errorCode = bundle.getInt(MyLocationStyle.ERROR_CODE);
+                String errorInfo = bundle.getString(MyLocationStyle.ERROR_INFO);
+                // 定位类型，可能为GPS WIFI等，具体可以参考官网的定位SDK介绍
+                int locationType = bundle.getInt(MyLocationStyle.LOCATION_TYPE);
+                LatLng latLng=new LatLng(location.getLatitude(),location.getLongitude());
+                setLocation(latLng);
+                /*
+                errorCode
+                errorInfo
+                locationType
+                */
+                Log.e("onMyLocationChange", "定位信息， code: " + errorCode + " errorInfo: " + errorInfo + " locationType: " + locationType);
+            } else {
+                Log.e("onMyLocationChange", "定位信息， bundle is null ");
+
+            }
+
+        } else {
+            Log.e("onMyLocationChange", "定位失败");
+        }
+    }
+
+    /**
+     * 将位置设置地图中心
+     *
+     * @param ll
+     */
+    private void setLocation(LatLng ll) {
+        if (ll_oilstatiion_contentLayout == null) {//修改app后台被系统销毁时 定位后视图null不处理
+            return;
+        }
+        //获取地图可视范围(地图的真实宽高目前等于[屏幕宽高])
+        int MapHeight = DensityUtil.dp2px(340);
+        int visibilityBoundH = MapHeight - ll_oilstatiion_contentLayout.getChildAt(0).getHeight(); //空出的地图区域(地图高-地图上卡片高);
+        final float offsetY = (float) (MapHeight * 0.5 - visibilityBoundH * 0.5f);
+        //重新定位
+        Point point = aMap.getProjection().toScreenLocation(ll);
+        point.y = (int) (point.y + offsetY - DensityUtil.dp2px(0));//（40dp）避免太靠上往下移一点
+
+        LatLng latLng = aMap.getProjection().fromScreenLocation(point);
+        aMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+    }
+
+    /**
+     * 防止AppBarLayout头部滑动不了，需要在数据加载出来后调用该方法
+     */
+    public void initAppbar() {
+        CoordinatorLayout.LayoutParams params = (CoordinatorLayout.LayoutParams) appbarHome.getLayoutParams();
+        AppBarLayout.Behavior behavior = (AppBarLayout.Behavior) params.getBehavior();
+        if (behavior != null) {
+            behavior.setDragCallback(new AppBarLayout.Behavior.DragCallback() {
+                @Override
+                public boolean canDrag(@NonNull AppBarLayout appBarLayout) {
+                    return true;
+                }
+            });
+        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        mv_gmap.onResume();
+        tmv_current_oilStation.onResume();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        mv_gmap.onPause();
+        tmv_current_oilStation.onPause();
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        mv_gmap.onSaveInstanceState(outState);
+        tmv_current_oilStation.onSaveInstanceState(outState);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        mv_gmap.onDestroy();
+        tmv_current_oilStation.onDestroy();
     }
 
     public static String sHA1(Context context) {
@@ -184,31 +361,4 @@ public class GMapActivity extends BaseActivity implements AMap.OnMyLocationChang
         return null;
     }
 
-    @Override
-    public void onMyLocationChange(Location location) {
-        // 定位回调监听
-        if (location != null) {
-            Log.e("onMyLocationChange", "onMyLocationChange 定位成功， lat: " + location.getLatitude() + " lon: " + location.getLongitude());
-            Bundle bundle = location.getExtras();
-            if (bundle != null) {
-                int errorCode = bundle.getInt(MyLocationStyle.ERROR_CODE);
-                String errorInfo = bundle.getString(MyLocationStyle.ERROR_INFO);
-                // 定位类型，可能为GPS WIFI等，具体可以参考官网的定位SDK介绍
-                int locationType = bundle.getInt(MyLocationStyle.LOCATION_TYPE);
-
-                /*
-                errorCode
-                errorInfo
-                locationType
-                */
-                Log.e("onMyLocationChange", "定位信息， code: " + errorCode + " errorInfo: " + errorInfo + " locationType: " + locationType);
-            } else {
-                Log.e("onMyLocationChange", "定位信息， bundle is null ");
-
-            }
-
-        } else {
-            Log.e("onMyLocationChange", "定位失败");
-        }
-    }
 }
